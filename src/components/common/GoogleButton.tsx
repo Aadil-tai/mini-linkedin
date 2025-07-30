5cf3da0c669398f49c29ad414b74966970cbb502
Your Name <60460219+Aadil-tai@users.noreply.github.com>
2025-07-31 04:02:12 +0530

    LatestPush

--- src/components/common/GoogleButton.tsx
+++ src/components/common/GoogleButton.tsx
@@ -11,11 +11,14 @@ export function GoogleButton({
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
       },
     });
   };
-
   return (
     <button
       onClick={handleGoogleSignIn}
